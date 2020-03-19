f51fd8a351c944c8acfd17244ae8cb57b1fca3db
zhuhh <zhuhh@asiainfo.com>
2020-03-19 22:13:55 +0800

    出版

---
+++ src/components/Demo.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <el-container></el-container>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "demo",
+  components: {},
+  data: {}
+};
+</script>
+
+<style>
+.el-row {
+  height: 500px;
+}
+</style>
